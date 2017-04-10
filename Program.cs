61aae5a5700cbdd8fc1998432971a4c6f278ce77
Andrew Froiland <andrew.froiland@gmail.com>
2017-04-10 15:45:20 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c__testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
