831aea8fbd6e2c56673b575dfd4534807304b3e0
Andrew Froiland <andrew.froiland@gmail.com>
2017-04-14 16:44:42 -0500

    if syntax

--- Program.cs
+++ Program.cs
@@ -7,18 +7,38 @@ namespace c__testing
     {
         static void Main(string[] args)
         {
-            string[] names = new string[2];
-            names[0] = "Billy";
-            names[1] = "Jimmy";
-            for (int i = 0; i < names.Length; i++) {
-                Console.WriteLine("Item number " + i + ": " + names[i]);
-            }
-
-
+            Console.WriteLine("enter the number one");
+            string userValue = Console.ReadLine();
+            string message = (userValue == "1") ? "success" : "failure";
+            Console.WriteLine("You have achieved " + message);
+            Console.ReadLine();
         }
 
 
-        // Compare the results of the following if "ref" is removed in each place:
+        // Easier way to create array, also Array.Sort():
+        //
+        // {
+        //     int[] numbers = {4, 3, 8, 0, 5};
+        //     Array.Sort(numbers);
+        //     foreach(int i in numbers)
+        //         Console.WriteLine(i);
+        // }
+
+
+        // This is a more elaborate way to create an array:
+        //
+        // {
+        //     string[] names = new string[2];
+        //     names[0] = "Billy";
+        //     names[1] = "Jimmy";
+        //     for (int i = 0; i < names.Length; i++) {
+        //         Console.WriteLine("Item number " + i + ": " + names[i]);
+        //     }
+        // }
+
+
+        // Compare the results of the following if "ref" is removed in both places:
+        //
         // {
         //     int number = 20;
         //     AddFive(ref number);
